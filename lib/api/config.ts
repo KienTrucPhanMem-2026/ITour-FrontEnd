b0345db7dc4c1232e36e7e0ab50c49947a9c9e78
nguyentrongnghia11 <nghianguyen15012004@gmail.com>
2026-05-29 16:03:58 +0700

    feat: implement homepage UI with search functionality and tour navigation

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -2,10 +2,13 @@
 // API Client — Base config với credentials:include để gửi HttpOnly cookie
 // ============================================================
 
-import { log } from "console";
 
-export const API_BASE_URL = "http://localhost:8080/api";
+console.log("Production ", process.env.NEXT_PUBLIC_ENVIRONMENT);
 
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION"
+  ? "http://54.254.171.18:8080/api"
+  : "http://localhost:8080/api";
 /** Lỗi từ API — chứa code, message */
 export class ApiError extends Error {
   constructor(
